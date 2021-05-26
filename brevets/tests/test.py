12d9794bf22cfad6232df69a90f4defa15f09130
cristianionion <63064438+cristianionion@users.noreply.github.com>
2021-05-25 19:00:16 -0700

    Delete brevets/tests directory

--- brevets/tests/test.py
+++
@@ -1,9 +0,0 @@
-#Nose tests for P5
-import logging
-import acp_times
-import flask_brevets
-
-logging.basicConfig(format='%(levelname)s:%(message)s',
-                    level=logging.WARNING)
-log = logging.getLogger(__name__)
-
